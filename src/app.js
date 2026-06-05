4f5764b508bbee3983d9353d59a733aee01c942c
jaipeee <jaipratapjaipee@gmail.com>
2026-06-06 04:31:17 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const app = express()
 const authRoutes = require('./routes/auth.routes')
 const leadRoute = require('./routes/lead.routes')
@@ -8,28 +9,24 @@ const googleRoutes = require('./routes/google.routes')
 const reportsRoutes = require('./routes/reports.routes')
 
 const allowedOrigins = [
-    process.env.CLIENT_UR,
+    process.env.CLIENT_URL,
     'http://localhost:5173',
     'http://127.0.0.1:5173'
-].filter(Boolean)
+].filter(Boolean);
 
-app.use((req, res, next) => {
-    const origin = req.headers.origin
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-    if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin)
-    }
-
-    res.header('Access-Control-Allow-Credentials', 'true')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
-
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(204)
-    }
-
-    next()
-})
 
 app.use(express.json());
 app.use(cookieParser());
